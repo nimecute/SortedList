753fb8c914a43997f4b7b6146904ebb9869b7f94
nimecute <Wongsakornnamsutia@gmail.com>
2022-06-22 01:12:14 +0700

    SortedList
    SortedList Code

---
+++ Collection/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Collection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SortedList sortedList;
+        public MainWindow()
+        {
+            InitializeComponent();
+            sortedList = new SortedList();
+        }
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            sortedList.Add(int.Parse(KeyTxt.Text),ValuTxt.Text);
+            ICollection collection = sortedList.Keys;
+            foreach (int key in collection)
+            {
+                for(int j = key; j == key;j++)
+                {
+                    MessageBox.Show(sortedList[key].ToString());
+                } 
+            }
+        }
+    }
+}
